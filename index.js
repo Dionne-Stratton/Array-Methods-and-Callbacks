d4adaced18ed0c68102b739caa6725ac80acc843
Dionne Stratton <mylife33@hotmail.com>
2020-08-18 15:23:55 -0500

    task two done

--- index.js
+++ index.js
@@ -69,11 +69,17 @@ taskOneE(fifaData);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
+function getFinals(data) {
+    let newArr = [];
+    data.forEach(element => {
+         if(element.Stage === 'Final'){
+           newArr.push(element);
+        }
+    });
+    return newArr;
 };
+
+console.log(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
